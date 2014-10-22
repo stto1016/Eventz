4c41f9a40eeed8fa9061900a8bbe6943a53d6843
Tobi <Tobi@Eventz.de>
2014-10-22 12:01:21 +0200

    INIT

---
+++ app/src/main/java/de/bpsz/android/eventz/SelectionFragment.java
@@ -0,0 +1,28 @@
+package de.bpsz.android.eventz;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SelectionFragment extends Fragment {
+
+    private static final String TAG = "SelectionFragment";
+
+    public SelectionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_selection, container, false);
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+}
